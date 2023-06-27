6fe364725f983d739339814981228e33c9f4b363
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2023-06-28 02:42:21 +0800

    feat: add countries constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,3 +9,32 @@ export * from './ysSubcategories';
 
 export const SITE_NAME = 'Beyond Ys';
 export const CATEGORIES_WITH_TIMELINE = ['Ys Series', 'Trails Series'] as const;
+
+// alphabetized by language
+export const COUNTRIES = [
+  {
+    name: 'China',
+    countryCode: 'CN',
+    language: 'Chinese',
+  },
+  {
+    name: 'United States',
+    countryCode: 'US',
+    language: 'English',
+  },
+  {
+    name: 'France',
+    countryCode: 'FR',
+    language: 'French',
+  },
+  {
+    name: 'Japan',
+    countryCode: 'JP',
+    language: 'Japanese',
+  },
+  {
+    name: 'South Korea',
+    countryCode: 'KR',
+    language: 'Korean',
+  },
+];
