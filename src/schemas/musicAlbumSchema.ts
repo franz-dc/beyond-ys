56e0c922fa3e04ea01d7b015ec1470c3f27cd205
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2023-07-18 02:03:44 +0800

    feat: update album schema

--- src/schemas/musicAlbumSchema.ts
+++ src/schemas/musicAlbumSchema.ts
@@ -8,6 +8,8 @@ export const musicAlbumSchema = z.object({
   // releaseDate is string because some albums might not have a release date
   // or have partial dates which is not supported by firestore's timestamp
   releaseDate: z.string().or(z.date()).nullable(),
+  // to prevent unnecessary requests
+  hasAlbumArt: z.boolean(),
   updatedAt: z.any(),
   // for reducing the amount of reads
   // to be used in the music (albums) page
@@ -18,5 +20,5 @@ export type MusicAlbumSchema = z.infer<typeof musicAlbumSchema>;
 
 export type MusicAlbumCacheSchema = Pick<
   MusicAlbumSchema,
-  'name' | 'releaseDate'
+  'name' | 'releaseDate' | 'hasAlbumArt'
 >;
