c546c57c5ca1f59fe33dd2dc1229aa09e442927e
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2023-07-19 22:57:40 +0800

    fix: cloud storage url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,7 @@ export * from './ysSubcategories';
 export const CLOUD_STORAGE_URL =
   process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true'
     ? `http://${process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_URL}:${process.env.NEXT_PUBLIC_STORAGE_EMULATOR_PORT}/${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`
-    : `https://firebasestorage.googleapis.com/${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`;
+    : `https://storage.googleapis.com/${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`;
 
 export const SITE_NAME = 'Beyond Ys';
 export const CATEGORIES_WITH_TIMELINE = ['Ys Series', 'Trails Series'] as const;
