eda3544024d63edfdfc780fe5784e99f54fa4ef3
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2023-09-22 18:12:27 +0800

    feat: update faq

--- src/pages/frequently-asked-questions/index.tsx
+++ src/pages/frequently-asked-questions/index.tsx
@@ -31,9 +31,9 @@ const Faqs = () => (
       to ensure up-to-date content.
     </FaqContainer>
     <FaqContainer question='How do I contribute?'>
-      As of now, there is no way to directly contribute to the contents of this
-      website yet. In the future, depending on the outcome of this project, it
-      might be implemented. In the meantime, you can email at{' '}
+      As of now, contributing to the contents of this website is limited. In the
+      future, depending on the outcome of this project, it might be fully
+      implemented for everyone. In the meantime, you can email at{' '}
       <Link
         href='mailto:hello@beyondys.com'
         target='_blank'
