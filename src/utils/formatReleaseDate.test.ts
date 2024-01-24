190f30302c95333d3b72b14fa9d179010e0a7161
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2024-01-24 21:39:35 +0800

    test: add formatReleaseDate test cases

---
+++ src/utils/formatReleaseDate.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+
+import { formatReleaseDate } from './formatReleaseDate';
+
+describe('formatReleaseDate', () => {
+  it('should return "Unknown release date" if dateStr is empty', () => {
+    expect(formatReleaseDate('')).toBe('Unknown release date');
+  });
+  it('should return "Unknown release date" if dateStr is not a valid date', () => {
+    expect(formatReleaseDate('foo')).toBe('Unknown release date');
+  });
+  it('should return the year if dateStr is a valid year', () => {
+    expect(formatReleaseDate('2021')).toBe('2021');
+  });
+  // month and day are not tested due to the use of Date.toLocaleString
+});
