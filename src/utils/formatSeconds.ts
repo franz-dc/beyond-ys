da6dd4787d89efe421c5438411cf827adf871b0a
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2024-01-24 16:13:58 +0800

    refactor: handle negative input on formatSeconds

--- src/utils/formatSeconds.ts
+++ src/utils/formatSeconds.ts
@@ -1,8 +1,13 @@
 export const formatSeconds = (seconds: number) => {
+  if (seconds < 0) {
+    return '0:00';
+  }
+
   const hours = Math.floor(seconds / 3600);
   const minutesLeft = Math.floor((seconds - hours * 3600) / 60);
   const secondsLeft = Math.floor(seconds - hours * 3600 - minutesLeft * 60);
-  return `${hours ? `${hours}:` : ''}${minutesLeft}:${secondsLeft
-    .toString()
-    .padStart(2, '0')}`;
+
+  return `${hours ? `${hours}:` : ''}${
+    hours ? minutesLeft.toString().padStart(2, '0') : minutesLeft
+  }:${secondsLeft.toString().padStart(2, '0')}`;
 };
