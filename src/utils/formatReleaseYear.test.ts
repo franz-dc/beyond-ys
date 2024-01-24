7aff9dfb403ba56e4060e4f296ca28b55aaf069f
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2024-01-24 21:05:25 +0800

    test: add formatReleaseYear test cases

---
+++ src/utils/formatReleaseYear.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+
+import { formatReleaseYear } from './formatReleaseYear';
+
+describe('formatReleaseYear', () => {
+  it('should return "Unknown release year" if dateStr is empty', () => {
+    expect(formatReleaseYear('')).toBe('Unknown release year');
+  });
+  it('should return "Unknown release year" if dateStr is not a valid date', () => {
+    expect(formatReleaseYear('foo')).toBe('Unknown release year');
+  });
+});
